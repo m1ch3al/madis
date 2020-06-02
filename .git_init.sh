f00913405fceae1799aafb16dcbcf524d9cb5eb8
Renato Sirola <renato.sirola@gmail.com>
2020-06-02 15:49:38 +0200

    minor changes

--- .git_init.sh
+++ .git_init.sh
@@ -1,3 +1,6 @@
 git init
 git remote add origin https://github.com/m1ch3al/madis.git
 
+git config credential.helper store
+git push https://github.com/m1ch3al/madis.git
+git config --global credential.helper 'cache --timeout 7200'
