58079e5c9f5d345d7234a45da69b9dc474601072
Renato Sirola <renato.sirola@gmail.com>
2020-06-10 23:35:36 +0200

    export first stage inside RasperryPI

--- src/madis/core/mad_main.py
+++ src/madis/core/mad_main.py
@@ -28,30 +28,29 @@ import subprocess
 
 def main():
     mad_configuration = read_mad_configuration("/home/micheal/.madics/mad-configuration.yaml")
-
     # Creates ROS publishers
+    preload()
     create_ros_nodes("/home/micheal/.madics/mad-configuration.yaml")
 
     while True:
         time.sleep(10)
 
 
-
-
-
 def create_ros_nodes(mad_configuration_filepath):
     blackboard = BlackBoard()
     mad_initial_configuration = blackboard.get_value("initial_configuration")
-
     node_configuration = read_ros_node_configuration()
     for single_ros_node in node_configuration:
         script_name = node_configuration[single_ros_node]["script-name"]
         subprocess.Popen(["python", script_name, single_ros_node, mad_configuration_filepath])
 
 
-
+def preload():
+    blackboard = BlackBoard()
+    initial_configuration = blackboard.get_value("initial_configuration")
+    if initial_configuration["preload"]["wait-at-boot"] is True:
+        time.sleep(initial_configuration["preload"]["seconds-to-wait"])
 
 
 if __name__ == "__main__":
     main()
-
