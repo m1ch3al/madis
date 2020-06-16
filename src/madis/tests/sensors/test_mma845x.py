de0245eb468f9e25ec6d31f8f20397d3f5b69d21
Renato Sirola <renato.sirola@gmail.com>
2020-06-16 19:43:21 +0200

    tests changed for mma845x - cycle

--- src/madis/tests/sensors/test_mma845x.py
+++ src/madis/tests/sensors/test_mma845x.py
@@ -1,15 +1,23 @@
 from madis.sensors.sensor_mma845x import MMA845XSensor
 import unittest
 import json
+import os
+import time
 
 
 class TestMMA845X(unittest.TestCase):
     def test_data(self):
+        cycles = 1000
+        counter = 0
         mma845x_sensor = MMA845XSensor()
         mma845x_sensor.initialize_sensor()
-        json_data = mma845x_sensor.read_from_sensor()
-        accelerometer_data = json.loads(json_data)
-        print(accelerometer_data)
+        while counter < cycles:
+            os.system("clear")
+            json_data = mma845x_sensor.read_from_sensor()
+            accelerometer_data = json.loads(json_data)
+            print(accelerometer_data)
+            counter += 1
+            time.sleep(0.1)
 
 
 if __name__ == '__main__':
